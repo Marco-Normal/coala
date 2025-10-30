7532000a66c6f8d7631235518d958d6da6d2471a
Marco Túlio <marcotulio2709@usp.br>
2025-10-29 22:28:33 -0300

    Diversas Mudanças

--- src/col_parser.rs
+++ src/col_parser.rs
@@ -1,35 +1,15 @@
 use datetime::datetime::Datetime;
 use log::info;
+use miette::{miette, Diagnostic, Error, IntoDiagnostic};
+use rand::{prelude::*, rng};
 use std::{
+    fmt::{self, Display},
     fs::File,
     io::{BufRead, BufReader},
     str::FromStr,
+    usize,
 };
 use thiserror::Error;
-
-use miette::{miette, Diagnostic, Error, IntoDiagnostic};
-
-struct CsvHandler;
-
-impl CsvHandler {
-    fn get_col_name(path: &str, col_number: usize, separator: char) -> Result<String, Error> {
-        let csv = File::open(path).into_diagnostic()?;
-        let reader = BufReader::new(csv);
-        let header = reader.lines().next();
-        match header {
-            Some(name) => {
-                let name = name.into_diagnostic()?;
-                let parts = name.split(separator).nth(col_number);
-                if let Some(parts) = parts {
-                    Ok(parts.to_string())
-                } else {
-                    Ok(String::from("Unnamed"))
-                }
-            }
-            None => Ok(String::from("Unnamed")),
-        }
-    }
-}
 
 #[derive(Debug)]
 pub(crate) struct CsvCol<T> {
@@ -49,13 +29,46 @@ enum ColParseError {
     #[error("Error in collumn named `{name}`. Invalid data type, couldn't match with any.")]
     InvalidColType { name: String },
 }
-#[derive(Debug)]
-#[derive(Default)]
-pub(crate) struct ColConfig {
-    pub(crate) date_format: Option<String>,
+#[derive(Debug, Default)]
+pub(crate) struct ColConfig<'a> {
+    pub(crate) date_format: Option<&'a str>,
     pub(crate) as_date: bool,
 }
 
+impl fmt::Display for ColType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Float(col) => {
+                writeln!(f, "{}", col.col_name)?;
+                for idx in 0..col.n_elements {
+                    writeln!(f, "{}", col.values[idx])?;
+                }
+                Ok(())
+            }
+            Self::Integer(col) => {
+                writeln!(f, "{}", col.col_name)?;
+                for idx in 0..col.n_elements {
+                    writeln!(f, "{}", col.values[idx])?;
+                }
+                Ok(())
+            }
+            Self::String(col) => {
+                writeln!(f, "{}", col.col_name)?;
+                for idx in 0..col.n_elements {
+                    writeln!(f, "{}", col.values[idx])?;
+                }
+                Ok(())
+            }
+            Self::Datetime(col) => {
+                writeln!(f, "{}", col.col_name)?;
+                for idx in 0..col.n_elements {
+                    writeln!(f, "{}", col.values[idx])?;
+                }
+                Ok(())
+            }
+        }
+    }
+}
 
 impl ColType {
     pub(crate) fn from_values(
@@ -100,6 +113,125 @@ impl ColType {
             _ => None,
         }
     }
+    pub(crate) fn print_range_lines(
+        &self,
+        beg: usize,
+        end: usize,
+    ) -> Result<(Vec<String>, usize), Error> {
+        match self {
+            Self::Float(col) => col.get_range_as_strings(beg, end),
+            Self::Integer(col) => col.get_range_as_strings(beg, end),
+            Self::Datetime(col) => col.get_range_as_strings(beg, end),
+            Self::String(col) => col.get_range_as_strings(beg, end),
+        }
+    }
+    pub(crate) fn name(&self) -> &str {
+        match self {
+            ColType::Float(csv_col) => &csv_col.col_name,
+            ColType::Integer(csv_col) => &csv_col.col_name,
+            ColType::String(csv_col) => &csv_col.col_name,
+            ColType::Datetime(csv_col) => &csv_col.col_name,
+        }
+    }
+    pub(crate) fn mean(&self) -> Option<DataValue> {
+        match self {
+            Self::Float(col) => {
+                let mean = col.values.iter().sum::<f64>();
+                dbg!(mean);
+                let mean = mean / col.values.len() as f64;
+                Some(DataValue::Float(mean))
+            }
+            Self::Integer(col) => {
+                let mean =
+                    col.values.iter().map(|val| *val as f64).sum::<f64>() / col.values.len() as f64;
+                Some(DataValue::Float(mean))
+            }
+            c => unreachable!("Col {c} doesn't have mean implemented"),
+        }
+    }
+    pub(crate) fn data_as_value(&self, index: usize) -> Option<DataValue> {
+        match self {
+            ColType::Float(csv_col) => csv_col.values.get(index).map(|v| DataValue::Float(*v)),
+            ColType::Integer(csv_col) => csv_col.values.get(index).map(|v| DataValue::Integer(*v)),
+            ColType::String(csv_col) => csv_col
+                .values
+                .get(index)
+                .map(|v| DataValue::String(v.clone())),
+            ColType::Datetime(csv_col) => {
+                csv_col.values.get(index).map(|v| DataValue::Datetime(*v))
+            }
+        }
+    }
+    pub(crate) fn quantile(&self, quantile: f64) -> Option<DataValue> {
+        match self {
+            ColType::Float(csv_col) => Some(DataValue::Float(csv_col.quantiles(quantile))),
+            ColType::Integer(csv_col) => Some(DataValue::Integer(csv_col.quantiles(quantile))),
+            ColType::Datetime(csv_col) => Some(DataValue::Datetime(csv_col.quantiles(quantile))),
+            ColType::String(_) => unreachable!("String doesn't have a median"),
+        }
+    }
+    pub(crate) fn median(&self) -> Option<DataValue> {
+        self.quantile(0.50)
+    }
+}
+
+impl<T: PartialOrd + Clone> CsvCol<T> {
+    fn quantiles(&self, quantile: f64) -> T {
+        _select_quantile(&self.values, quantile, |list| -> usize {
+            let mut rng = rng();
+            rng.random_range(0..list.len())
+        })
+    }
+}
+
+fn _select_quantile<T: PartialOrd + Clone>(
+    list: &[T],
+    quantile: f64,
+    pivot_selection: fn(&[T]) -> usize,
+) -> T {
+    assert!(quantile > 0.0 && quantile < 1.0);
+    let index = (1.0 / quantile) as usize;
+    _quick_select(list, index, pivot_selection)
+}
+
+fn _quick_select<T: PartialOrd + Clone>(
+    list: &[T],
+    index: usize,
+    pivot_selection: fn(&[T]) -> usize,
+) -> T {
+    if list.len() == 1 {
+        assert_eq!(index, 0);
+        return list[0].clone();
+    }
+    let pivot = pivot_selection(list);
+    let pivot = &list[pivot];
+    let lows: Vec<_> = list.iter().filter(|&x| x < pivot).cloned().collect();
+    let highs: Vec<_> = list.iter().filter(|&x| x > pivot).cloned().collect();
+    let pivots: Vec<_> = list.iter().filter(|&x| x == pivot).cloned().collect();
+    if index < lows.len() {
+        return _quick_select(&lows, index, pivot_selection);
+    } else if index < lows.len() + pivots.len() {
+        return pivots[0].clone();
+    }
+    _quick_select(&highs, index - lows.len() - pivots.len(), pivot_selection)
+}
+
+impl<T: Display> CsvCol<T> {
+    fn get_range_as_strings(&self, beg: usize, end: usize) -> Result<(Vec<String>, usize), Error> {
+        if end > self.n_elements || beg > end {
+            return Err(miette!("n is greater than number of lines in col"));
+        }
+        let mut max_width = 0;
+        let mut strings = Vec::with_capacity(end - beg);
+        for i in beg..end {
+            let s = self.values[i].to_string();
+            if s.len() > max_width {
+                max_width = s.len();
+            }
+            strings.push(s);
+        }
+        Ok((strings, max_width))
+    }
 }
 
 impl<T: FromStr> CsvCol<T> {
@@ -123,6 +255,7 @@ impl<T: FromStr> CsvCol<T> {
         })
     }
 }
+
 impl CsvCol<Datetime> {
     fn as_datetime(elements: &[String], name: &str, format: Option<&str>) -> Result<Self, Error> {
         let mut values = Vec::new();
@@ -155,4 +288,13 @@ impl CsvCol<Datetime> {
             values,
         })
     }
+}
+
+#[derive(Debug, Clone)]
+pub enum DataValue {
+    Float(f64),
+    Integer(i64),
+    String(String),
+    Datetime(Datetime),
+    Null,
 }
--- src/csv_parser.rs
+++ src/csv_parser.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -6,24 +7,97 @@ use std::{
 use miette::{miette, Diagnostic, Error, IntoDiagnostic};
 use thiserror::Error;
 
-use crate::col_parser::{ColConfig, ColType};
+use crate::col_parser::{ColConfig, ColType, DataValue};
 
 #[derive(Debug)]
 pub struct Csv {
     cols: Vec<ColType>,
     n_cols: usize,
+    n_rows: usize,
+    header: Vec<String>,
+    cache: HashMap<String, Statistics>,
 }
 
-pub struct CsvConfig {
+#[derive(Debug, Default)]
+struct Statistics {
+    mean: Option<DataValue>,
+    median: Option<DataValue>,
+    std_dev: Option<DataValue>,
+}
+
+pub struct ColViewer<'a> {
+    pub(crate) inner: &'a ColType,
+}
+
+impl<'a> ColViewer<'a> {
+    pub(crate) fn new(col: &'a ColType) -> Self {
+        Self { inner: col }
+    }
+    pub fn name(&self) -> &str {
+        self.inner.name()
+    }
+    pub fn mean(&self) -> Option<DataValue> {
+        self.inner.mean()
+    }
+    pub fn get(&self, index: usize) -> Option<DataValue> {
+        self.inner.data_as_value(index)
+    }
+    pub fn quantile(&self, quantile: f64) -> Option<DataValue> {
+        self.inner.quantile(quantile)
+    }
+    pub fn median(&self) -> Option<DataValue> {
+        self.inner.median()
+    }
+}
+
+pub struct CsvConfig<'a> {
     pub separator: char,
     pub header: Option<usize>,
-    pub parser_as_date: Option<Vec<String>>,
+    pub parser_as_date: Option<HashMap<String, Option<&'a str>>>,
 }
 #[derive(Debug, Diagnostic, Error)]
 enum ColParserError {
     #[error("Csv unexpectely ended")]
     UnexpectedEOF,
+    #[error(
+        "Number of lines to print (`{}`) is greater than DataFrame len (`{}`)",
+        n,
+        len
+    )]
+    OutOfLines { n: usize, len: usize },
+    #[error("Column `{}` not found in Dataframe", name)]
+    MissingCol { name: String },
+    #[error(
+        "Column `{}` doesn't have a datatype where `{}` can be calculated",
+        name,
+        metric
+    )]
+    InvalidMetric { name: String, metric: String },
 }
+
+macro_rules! statistics {
+        ($($t:ident)*) => ($(
+            pub fn $t(&mut self, name:&str) -> Result<DataValue, Error> {
+                if let Some(cache) = self.cache.get(name) {
+                    if let Some($t) = &cache.$t {
+                       return Ok($t.clone());
+                    }
+                }
+
+            let col = self
+            .get_col(name)
+            .ok_or_else(|| ColParserError::MissingCol {
+                name: name.to_string(),
+            })?;
+        let $t = col.$t().ok_or_else(|| ColParserError::InvalidMetric {
+            name: name.to_string(),
+            metric: String::from("$t"),
+        })?;
+                self.cache.entry(col.name().to_string()).or_default().$t = Some($t.clone());
+                Ok($t)
+            }
+        )*)
+    }
 impl Csv {
     pub fn new(path: &str, config: CsvConfig) -> Result<Self, Error> {
         let csv = File::open(path).into_diagnostic()?;
@@ -60,7 +134,6 @@ impl Csv {
                 }
             })
             .collect::<Result<Vec<_>, _>>()?;
-        let n_rows = values.len();
         let mut row_iters = values.into_iter().map(Vec::into_iter).collect::<Vec<_>>();
         let transposed: Vec<Vec<String>> = (0..n_cols)
             .map(|_| {
@@ -70,31 +143,94 @@ impl Csv {
                     .collect()
             })
             .collect();
-        dbg!(&transposed);
+        let n_rows = transposed[0].len();
         let mut cols: Vec<ColType> = Vec::with_capacity(n_cols);
         for (i, col_data) in transposed.into_iter().enumerate() {
             let col_name = header
                 .get(i)
                 .cloned()
                 .unwrap_or_else(|| format!("Unnamed: {i}"));
-            if let Some(ref cols_as_date) = config.parser_as_date {
-                if cols_as_date
-                    .iter()
-                    .any(|name| matches!(name, col_name))
-                {
-                    cols.push(ColType::from_values(
-                        &col_data,
-                        col_name,
-                        Some(ColConfig {
-                            as_date: true,
-                            ..Default::default()
-                        }),
-                    )?);
+            let config = if let Some(ref cols_as_date) = config.parser_as_date {
+                if cols_as_date.contains_key(&col_name) {
+                    Some(ColConfig {
+                        as_date: true,
+                        date_format: if let Some(&date_format) = cols_as_date.get(&col_name) {
+                            date_format
+                        } else {
+                            None
+                        },
+                    })
+                } else {
+                    None
                 }
             } else {
-                todo!()
+                None
             };
+            cols.push(ColType::from_values(&col_data, col_name, config)?);
         }
-        todo!()
+        Ok(Self {
+            cols,
+            n_cols,
+            n_rows,
+            header,
+            cache: Default::default(),
+        })
     }
+    fn print_n_lines(&self, beg: usize, end: usize) -> Result<String, Error> {
+        if beg > self.n_rows {
+            return Err(miette!(
+                "Number of rows to print is greater than len of dataset"
+            ));
+        }
+        let cols = self
+            .cols
+            .iter()
+            .map(|col| col.print_range_lines(beg, end))
+            .collect::<Result<Vec<_>, _>>()?;
+        let (cols, mut max_widths): (Vec<_>, Vec<_>) = cols.into_iter().unzip();
+        dbg!(&cols);
+        let mut result = String::new();
+        for (i, header) in self.header.iter().enumerate() {
+            max_widths[i] = max_widths[i].max(self.header[i].len());
+            result.push_str(&format!("{:<width$}", header, width = max_widths[i]));
+            if i < self.n_cols - 1 {
+                result.push_str(", ");
+            }
+        }
+        result.push('\n');
+        for row_idx in 0..(end - beg) {
+            for col_idx in 0..self.n_cols {
+                let value = &cols[col_idx][row_idx];
+                result.push_str(&format!("{:<width$}", value, width = max_widths[col_idx]));
+                if col_idx < self.n_cols - 1 {
+                    result.push_str(", ");
+                }
+            }
+            result.push('\n')
+        }
+        Ok(result)
+    }
+    pub fn head(&self) -> Result<(), Error> {
+        self.head_n(5)
+    }
+    pub fn head_n(&self, n_lines: usize) -> Result<(), Error> {
+        if self.n_rows < n_lines {
+            return Err(ColParserError::OutOfLines {
+                n: n_lines,
+                len: self.n_rows,
+            }
+            .into());
+        }
+        let result = self.print_n_lines(0, n_lines)?;
+        println!("{result}");
+        Ok(())
+    }
+    pub fn get_col(&self, name: &str) -> Option<ColViewer<'_>> {
+        self.cols
+            .iter()
+            .find(|c| c.name() == name)
+            .map(ColViewer::new)
+    }
+
+    statistics! {mean median }
 }
